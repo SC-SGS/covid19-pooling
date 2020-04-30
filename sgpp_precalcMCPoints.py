f82259fad04f72ca9273fe82e812b15df3e5c842
rehmemk <michael.rehme@ipvs.uni-stuttgart.de>
2020-04-30 10:55:47 +0200

    fake website

--- sgpp_precalcMCPoints.py
+++ sgpp_precalcMCPoints.py
@@ -21,7 +21,7 @@ regular_dict = {}
 for key in multiprocessing_dict:
     regular_dict[key] = multiprocessing_dict[key]
 
-filename = f'precalc/values/mc{numMCPoints}_{test_strategy}_{dim}dim_{qoi}.pkl'
+filename = f'precalc/values/mc{numMCPoints}_{test_strategy}_{dim}dim.pkl'
 with open(filename, 'wb+') as fp:
     pickle.dump(regular_dict, fp)
 
