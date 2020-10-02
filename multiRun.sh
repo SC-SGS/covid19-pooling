11330f852ff88112539209edc31d6c5dbf15d419
rehmemk <michael.rehme@ipvs.uni-stuttgart.de>
2020-10-02 16:03:27 +0200

    new stuff from sg_surrogate which is surrogate independent

--- multiRun.sh
+++
@@ -1,7 +0,0 @@
-LEVEL=3
-#python3 sgpp_precalcSparseGridPoints.py --test_strategy='individual-testing' --level=$LEVEL;
-python3 sgpp_precalcSparseGridPoints.py --test_strategy='two-stage-testing' --level=$LEVEL;
-#python3 sgpp_precalcSparseGridPoints.py --test_strategy='binary-splitting' --level=$LEVEL;
-#python3 sgpp_precalcSparseGridPoints.py --test_strategy='RBS' --level=$LEVEL;
-#python3 sgpp_precalcSparseGridPoints.py --test_strategy='purim' --level=$LEVEL;
-#python3 sgpp_precalcSparseGridPoints.py --test_strategy='sobel' --level=$LEVEL;
